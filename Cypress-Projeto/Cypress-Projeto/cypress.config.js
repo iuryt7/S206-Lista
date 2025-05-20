0e80e763fc1aed73f08f09b607c3a559ea1d09fc
PauloVPorto <paulo.v@gec.inatel.br>
2025-05-20 16:49:57 -0300

    Update cypress.config.js

--- Cypress-Projeto/Cypress-Projeto/cypress.config.js
+++ Cypress-Projeto/Cypress-Projeto/cypress.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-};
+    baseUrl: 'https://confianopai.com',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+  }
+})
